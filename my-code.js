1c3b830074939d912e6945d43166dca389657bf0
AndyD15 <Durrant.Andrea15@gmail.com>
2016-09-26 13:30:13 -0600

    Add files via upload

---
+++ my-code.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	
+	$("p").hide();
+	$("#bookPic").hide();
+	
+	$("h1").click(function() {
+
+		$(this).next().fadeToggle(1000);
+
+	});
+
+/* start of jq02 how to use buttons and selectors */
+
+$("#testbutton").click(function(){
+	
+	$("#jq02").css("background-color","#0000FF");
+	
+
+
+
+});
